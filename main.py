b55a5cd43d903dd1e5e8515568dcd32c4564a084
Dadaxon <dadaxonmasharibov@gmail.com>
2026-03-20 23:47:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -350,6 +350,7 @@ async def startup_event():
 #     port = int(os.getenv("PORT", 8000))
 #     uvicorn.run("main:app", host="0.0.0.0", port=port)
 
-# Yangi
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+
+
